9a1f47d24dabd37a131da51e81d3d939f233ee9e
Yusuke Asai <ptx014@gmail.com>
2024-08-04 13:52:16 +0900

    ViewModel作成中

---
+++ PresentationLayer/PracticeViewModels/Calc/CalcViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Reactive.Disposables;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace PracticeViewModels.Calc
+{
+    public class CalcViewModel : BindableBase, IDisposable
+    {
+        #region フィールド・プロパティ
+
+        #region 定数
+        #endregion
+
+        private readonly CompositeDisposable disposables = [];
+
+        #region ReactiveProperty
+        /// <summary>単価</summary>
+        public ReactivePropertySlim<string> UnitPrice { get; set; } = new ReactivePropertySlim<string>("1000");
+        /// <summary>数量</summary>
+        public ReactivePropertySlim<string> Piece { get; set; } = new ReactivePropertySlim<string>("10");
+        /// <summary>割引率</summary>
+        public ReactivePropertySlim<string> DiscountRate { get; set; } = new ReactivePropertySlim<string>("0");
+        /// <summary>実行中かどうか</summary>
+        public ReactivePropertySlim<bool> IsWorking { get; set; } = new ReactivePropertySlim<bool>();
+
+        /// <summary>計算開始</summary>
+        public AsyncReactiveCommand CommandExecute { get; set; } = new AsyncReactiveCommand();
+
+        #endregion
+
+        #endregion
+
+        #region 公開メソッド
+        public CalcViewModel()
+        {
+            _ = this.CommandExecute.Subscribe(this.OnCommandExecute).AddTo(this.disposables);
+        }
+
+        public void Dispose()
+        {
+            this.disposables.Clear();
+        }
+        #endregion
+
+        #region コールバック
+
+        private async Task OnCommandExecute()
+        {
+            // asyncに警告を出さないためのおまじない
+            await Task.Delay(0);
+        }
+
+        #endregion
+
+        #region 非公開メソッド
+
+        private async Task Calc()
+        {
+            
+        }
+
+        #endregion
+
+
+    }
+}
