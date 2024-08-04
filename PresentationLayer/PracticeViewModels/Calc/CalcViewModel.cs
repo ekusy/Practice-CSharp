eacfbf9cffef2349b9918c86aec82f9fa29e7ff6
Yusuke Asai <ptx014@gmail.com>
2024-08-04 16:00:37 +0900

    ViewModelの入力部分作成

--- PresentationLayer/PracticeViewModels/Calc/CalcViewModel.cs
+++ PresentationLayer/PracticeViewModels/Calc/CalcViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reactive.Disposables;
+using System.Windows;
 using Prism.Mvvm;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
@@ -56,6 +57,72 @@ namespace PracticeViewModels.Calc
         #endregion
 
         #region 非公開メソッド
+
+        private void Calc()
+        {
+            // 入力パラメータを集める
+            var unitPrice = 0;
+            var amount = 0;
+            var discountRate = 0.0;
+
+            try
+            {
+                unitPrice = this.GetPrice();
+                amount = this.GetAmount();
+                discountRate = this.GetDiscountRate();
+            }
+            catch(ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            // 合計金額計算ユースケースを呼び出す
+        }
+
+        private int GetPrice()
+        {
+            if(!int.TryParse(this.UnitPrice.Value, out var value))
+            {
+                throw new ArgumentException("単価は0より大きい数値を設定してください");
+            }
+
+            if(value <= 0)
+            {
+                throw new ArgumentException("単価は0より大きい数値を設定してください");
+            }
+
+            return value;
+        }
+
+        private int GetAmount()
+        {
+            if (!int.TryParse(this.Amount.Value, out var value))
+            {
+                throw new ArgumentException("個数は0より大きい数値を設定してください");
+            }
+
+            if (value <= 0)
+            {
+                throw new ArgumentException("個数は0より大きい数値を設定してください");
+            }
+
+            return value;
+        }
+
+        private double GetDiscountRate()
+        {
+            if (!int.TryParse(this.DiscountRate.Value, out var value))
+            {
+                throw new ArgumentException("割引率には数値を設定してください");
+            }
+
+            if (value <= 0)
+            {
+                throw new ArgumentException("個数は0より大きい数値を設定してください");
+            }
+
+            return value;
+        }
 
         #endregion
 
