97f978d9250486f406d88e5f2a913c5853eb4e3c
Yusuke Asai <ptx014@gmail.com>
2024-11-02 09:48:16 +0900

    電卓アプリの雛形作成

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return this.Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            ;
+        }
+    }
+
+}
