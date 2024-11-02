76c8f20c4a9da64d804ac9b50608fe40d934bd78
Yusuke Asai <ptx014@gmail.com>
2024-11-02 11:06:34 +0900

    ReactiveCommandのサンプル作成

--- Calculator/CalculatorViewModels/CalculatorViewModel.cs
+++ Calculator/CalculatorViewModels/CalculatorViewModel.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using Prism.Mvvm;
 using Reactive.Bindings;
 
@@ -5,7 +6,27 @@ namespace CalculatorViewModels
 {
     public class CalculatorViewModel : BindableBase
     {
+        #region フィールド・プロパティ
         public ReactivePropertySlim<string> Test { get; set; } = new ReactivePropertySlim<string>("電卓アプリ");
+
+        public ReactiveCommand CommandButtonEqual { get; set; } = new ReactiveCommand();
+        #endregion
+
+        #region コンストラクタ
+        public CalculatorViewModel()
+        {
+            _ = this.CommandButtonEqual.Subscribe(this.OnCommandButtonEqual);
+        }
+
+        #endregion
+
+        #region privateメソッド
+        private void OnCommandButtonEqual()
+        {
+            //MessageBox.Show("\"=\"が押されました");
+            this.Test.Value = Guid.NewGuid().ToString();
+        }
+        #endregion
     }
 
 }
